1f81089cd0594e690d36459a61f1c16c524a899d
Dawid Skarwecki <dawidskarwecki@gmail.com>
2022-01-29 16:44:17 +0100

    Add booking class

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,57 @@
-import { settings, select } from './settings.js'; //ten sam katalog
+import { settings, select, classNames } from './settings.js'; //ten sam katalog
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
+import Booking from './components/Booking.js';
 const app = {
+  initBooking: function () {
+    const thisBooking = this;
+    thisBooking.bookContainer = document.querySelector(
+      select.containerOf.booking
+    );
+    const book = new Booking(thisBooking.bookContainer);
+    return book;
+  },
+  initPages: function () {
+    const thisApp = this;
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);
+    const idFromHash = window.location.hash.replace('#/', '');
+
+    let pageMatchingHash = thisApp.pages[0].id;
+    for (let page of thisApp.pages) {
+      if (page.id == idFromHash) {
+        pageMatchingHash = page.id;
+        break; //brak kolejnych iteracji petli
+      }
+    }
+    thisApp.activatePage(pageMatchingHash);
+    for (let link of thisApp.navLinks) {
+      link.addEventListener('click', function (event) {
+        const clickedElemend = this;
+        event.preventDefault();
+        /*get page id from href attribute */
+        const id = clickedElemend.getAttribute('href').replace('#', '');
+        /*run thisApp.activatePage with that id */
+        thisApp.activatePage(id);
+        /*change URL hash */
+        window.location.hash = '#/' + id;
+      });
+    }
+  },
+  activatePage: function (pageId) {
+    const thisApp = this;
+    /*add class active to matching pages, remove from non-matching*/
+    for (let page of thisApp.pages) {
+      page.classList.toggle(classNames.pages.active, page.id == pageId);
+    }
+    /*add class active to matching links, remove from non-matching*/
+    for (let link of thisApp.navLinks) {
+      link.classList.toggle(
+        classNames.nav.active,
+        link.getAttribute('href') == '#' + pageId
+      );
+    }
+  },
   initMenu: function () {
     const thisApp = this;
     console.log('thisApp.data', thisApp.data);
@@ -46,6 +96,8 @@ const app = {
     thisApp.initData();
 
     thisApp.initCart();
+    thisApp.initPages();
+    thisApp.initBooking();
   },
 };
 
--- src/js/settings.js
+++ src/js/settings.js
@@ -2,10 +2,13 @@ export const select = {
   templateOf: {
     menuProduct: '#template-menu-product',
     cartProduct: '#template-cart-product', // CODE ADDED
+    bookingWidget: '#template-booking-widget',
   },
   containerOf: {
     menu: '#product-list',
     cart: '#cart',
+    pages: '#pages',
+    booking: '.booking-wrapper',
   },
   all: {
     menuProducts: '#product-list > .product',
@@ -27,6 +30,24 @@ export const select = {
       linkIncrease: 'a[href="#more"]',
     },
   },
+  datePicker: {
+    wrapper: '.date-picker',
+    input: `input[name="date"]`,
+  },
+  hourPicker: {
+    wrapper: '.hour-picker',
+    input: 'input[type="range"]',
+    output: '.output',
+  },
+  booking: {
+    peopleAmount: '.people-amount',
+    hoursAmount: '.hours-amount',
+    tables: '.floor-plan .table',
+  },
+  nav: {
+    links: '.main-nav a',
+  },
+
   // CODE ADDED START
   cart: {
     productList: '.cart__order-summary',
@@ -59,6 +80,16 @@ export const classNames = {
   cart: {
     wrapperActive: 'active',
   },
+  booking: {
+    loading: 'loading',
+    tableBooked: 'booked',
+  },
+  nav: {
+    active: 'active',
+  },
+  pages: {
+    active: 'active',
+  },
   // CODE ADDED END
 };
 
@@ -73,10 +104,26 @@ export const settings = {
     defaultDeliveryFee: 20,
   },
   // CODE ADDED END
+  hours: {
+    open: 12,
+    close: 24,
+  },
+  datePicker: {
+    maxDaysInFuture: 14,
+  },
+  booking: {
+    tableIdAttribute: 'data-table',
+  },
   db: {
     url: '//localhost:3131',
     products: 'products',
-    orders: 'orders',
+    order: 'order',
+    booking: 'booking',
+    event: 'event',
+    dateStartParamKey: 'date_gte',
+    dateEndParamKey: 'date_lte',
+    notRepeatParam: 'repeat=false',
+    repeatParam: 'repeat_ne=false',
   },
 };
 
@@ -89,4 +136,7 @@ export const templates = {
     document.querySelector(select.templateOf.cartProduct).innerHTML
   ),
   // CODE ADDED END
+  bookingWidget: Handlebars.compile(
+    document.querySelector(select.templateOf.bookingWidget).innerHTML
+  ),
 };
